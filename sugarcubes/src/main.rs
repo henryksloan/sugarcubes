3502c2c1634defe8dad28b9b3a218bc9d50cdce4
Henry Sloan <henryksloan@gmail.com>
2021-06-01 17:08:22 -0400

    Implement adding new transitions

--- sugarcubes/src/main.rs
+++ sugarcubes/src/main.rs
@@ -6,6 +6,18 @@ use sugarcubes_core::automata::{
 use macroquad::prelude::*;
 use std::collections::HashMap;
 
+const INACTIVE_COLOR: Color = Color::new(0.90, 0.93, 0.52, 1.00);
+const ACTIVE_COLOR: Color = Color::new(0.44, 0.45, 0.19, 1.00);
+const SELECTED_COLOR: Color = Color::new(0.45, 0.58, 0.81, 1.00);
+
+const DOUBLE_CLICK_DELAY: f64 = 0.25;
+
+const STATE_RADIUS: f32 = 35.;
+const INITIAL_ARROW_SIZE: f32 = 24.;
+const ARROW_SIZE: f32 = 17.;
+const TRANSITION_FONT_SIZE: f64 = 24.;
+const STATE_FONT_SIZE: f64 = 30.;
+
 // Draw an arrow with its tip at a given point,
 // at a given angle relative to the horizontal,
 // and with a given sidelength
@@ -25,11 +37,70 @@ fn draw_arrow(point: Vec2, angle: f32, size: f32, outlined: bool) {
     }
 }
 
-const INACTIVE_COLOR: Color = Color::new(0.90, 0.93, 0.52, 1.00);
-const ACTIVE_COLOR: Color = Color::new(0.44, 0.45, 0.19, 1.00);
-const SELECTED_COLOR: Color = Color::new(0.45, 0.58, 0.81, 1.00);
+fn draw_transition(from: &Vec2, to: &Vec2, to_state: bool) {
+    let angle = vec2(1., 0.).angle_between(*to - *from);
+    let distance = from.distance(*to);
+    let radius_over_distance = STATE_RADIUS / distance;
+    let point_from = from.lerp(*to, radius_over_distance);
+    let point_to = to.lerp(*from, if to_state { radius_over_distance } else { 0. });
+    draw_line(
+        point_from.x,
+        point_from.y,
+        point_to.x,
+        point_to.y,
+        2.,
+        BLACK,
+    );
+    draw_arrow(point_to, angle, ARROW_SIZE, false);
+}
 
-const DOUBLE_CLICK_DELAY: f64 = 0.25;
+fn draw_transition_with_text(
+    from: &Vec2,
+    to: &Vec2,
+    to_state: bool,
+    text: String,
+    gl: &mut QuadGl,
+    font: &Font,
+) {
+    draw_transition(from, to, to_state);
+    draw_transition_text(from, to, text, gl, font);
+}
+
+fn draw_transition_text(from: &Vec2, to: &Vec2, text: String, gl: &mut QuadGl, font: &Font) {
+    // Change coordinate systems to be centered on the middle of the transition,
+    // and rotated parallel to the transition, then draw the text
+    let angle = vec2(1., 0.).angle_between(*to - *from);
+    let middle = from.lerp(*to, 0.5);
+    let font_size = TRANSITION_FONT_SIZE * 5.;
+    let text_size = measure_text(&text, None, font_size as _, 0.2);
+    gl.push_model_matrix(glam::Mat4::from_translation(glam::vec3(
+        middle.x, middle.y, 0.,
+    )));
+    let text_angle = angle
+        + if angle > std::f32::consts::FRAC_PI_2 || angle < -std::f32::consts::FRAC_PI_2 {
+            std::f32::consts::PI
+        } else {
+            0.
+        };
+    gl.push_model_matrix(glam::Mat4::from_rotation_z(text_angle));
+
+    draw_text_ex(
+        &text,
+        -text_size.width / 2.,
+        -8.,
+        TextParams {
+            font_size: font_size as _,
+            font_scale: 0.2,
+            font: *font,
+            color: BLACK,
+            ..Default::default()
+        },
+    );
+
+    // Reset the coordinate system
+    gl.pop_model_matrix();
+    gl.pop_model_matrix();
+}
 
 #[macroquad::main("Sugarcubes")]
 async fn main() {
@@ -52,10 +123,6 @@ async fn main() {
     position_map.insert(state1, vec2(400., 200.));
     position_map.insert(state2, vec2(400., 400.));
 
-    let radius = 35.;
-    let initial_arrow_size = 24.;
-    let arrow_size = 17.;
-
     let gl = unsafe { get_internal_gl().quad_gl };
 
     let font = load_ttf_font("./assets/OpenSans-Regular.ttf").await;
@@ -65,28 +132,33 @@ async fn main() {
     // so that the mouse "grabs" the state at the point of the initial click
     let mut state_drag_offset = Vec2::ZERO;
 
+    // If the user is drawing a new transition starting on a state, its ID is in here
+    let mut creating_transition_from: Option<u32> = None;
+
     let mut last_click_time = 0.;
 
     loop {
         clear_background(WHITE);
 
         // Process keys, mouse etc.
+        // TODO: Ignore clicks on egui elements
         let mouse_position: Vec2 = mouse_position().into();
         if is_mouse_button_pressed(MouseButton::Left) {
             let new_click_time = get_time();
             // Check for double click
             if last_click_time > 0. && new_click_time - last_click_time <= DOUBLE_CLICK_DELAY {
-                let mut state_double_clicked_on = None;
+                creating_transition_from = None;
+
                 // Iterate in reverse, so the highest-numbered state is selected first
                 for state in fa.automaton.states_iter().rev() {
                     let position = *position_map.get(state).unwrap();
-                    if mouse_position.abs_diff_eq(position, radius) {
-                        state_double_clicked_on = Some(*state);
+                    if mouse_position.abs_diff_eq(position, STATE_RADIUS) {
+                        creating_transition_from = Some(*state);
                         break;
                     }
                 }
 
-                if state_double_clicked_on.is_none() {
+                if creating_transition_from.is_none() {
                     let new_state = fa.automaton.add_new_state();
                     position_map.insert(new_state, mouse_position);
                     selected_state = Some(new_state);
@@ -95,7 +167,7 @@ async fn main() {
             } else {
                 for state in fa.automaton.states_iter().rev() {
                     let position = *position_map.get(state).unwrap();
-                    if mouse_position.abs_diff_eq(position, radius) {
+                    if mouse_position.abs_diff_eq(position, STATE_RADIUS) {
                         selected_state = Some(*state);
                         state_drag_offset = position - mouse_position;
                         break;
@@ -103,8 +175,28 @@ async fn main() {
                 }
             }
             last_click_time = new_click_time;
-        } else if !is_mouse_button_down(MouseButton::Left) {
+        }
+
+        if is_mouse_button_released(MouseButton::Left) {
             selected_state = None;
+
+            // If the user releases over a state while creating a transition,
+            // connect the two states
+            if let Some(from) = creating_transition_from {
+                for &other in fa.automaton.states_iter().rev() {
+                    let position = *position_map.get(&other).unwrap();
+                    if mouse_position.abs_diff_eq(position, STATE_RADIUS) {
+                        fa.automaton.add_transition(FiniteAutomatonTransition::new(
+                            from,
+                            other,
+                            EMPTY_STRING, // TODO: Add a symbol (at an angle?)
+                        ));
+                        break;
+                    }
+                }
+            }
+
+            creating_transition_from = None;
         }
 
         if is_mouse_button_pressed(MouseButton::Right) {
@@ -148,56 +240,14 @@ async fn main() {
 
             for transition in fa.automaton.transitions_from(*state) {
                 let other_position = position_map.get(&transition.to()).unwrap_or(&Vec2::ZERO);
-                let angle = vec2(1., 0.).angle_between(*other_position - *position);
-                let distance = position.distance(*other_position);
-                let radius_over_distance = radius / distance;
-                let point_from = position.lerp(*other_position, radius_over_distance);
-                let point_to = other_position.lerp(*position, radius_over_distance);
-                draw_line(
-                    point_from.x,
-                    point_from.y,
-                    point_to.x,
-                    point_to.y,
-                    2.,
-                    BLACK,
-                );
-
-                draw_arrow(point_to, angle, arrow_size, false);
-
-                // Change coordinate systems to be centered on the middle of the transition,
-                // and rotated parallel to the transition, then draw the text
-                let middle = position.lerp(*other_position, 0.5);
-                let font_size = 120.;
-                let symbol_str = &transition.symbol().to_string();
-                let text_size = measure_text(symbol_str, None, font_size as _, 0.2);
-                gl.push_model_matrix(glam::Mat4::from_translation(glam::vec3(
-                    middle.x, middle.y, 0.,
-                )));
-                let text_angle = angle
-                    + if angle > std::f32::consts::FRAC_PI_2 || angle < -std::f32::consts::FRAC_PI_2
-                    {
-                        std::f32::consts::PI
-                    } else {
-                        0.
-                    };
-                gl.push_model_matrix(glam::Mat4::from_rotation_z(text_angle));
-
-                draw_text_ex(
-                    symbol_str,
-                    -text_size.width / 2.,
-                    -8.,
-                    TextParams {
-                        font_size: font_size as _,
-                        font_scale: 0.2,
-                        font,
-                        color: BLACK,
-                        ..Default::default()
-                    },
-                );
-
-                // Reset the coordinate system
-                gl.pop_model_matrix();
-                gl.pop_model_matrix();
+                draw_transition_with_text(
+                    position,
+                    other_position,
+                    true,
+                    transition.symbol().to_string(),
+                    gl,
+                    &font,
+                )
             }
 
             let state_color = if selected_state == Some(*state) {
@@ -210,32 +260,36 @@ async fn main() {
             } else {
                 INACTIVE_COLOR
             };
-            draw_circle(position.x, position.y, radius, state_color);
-            draw_circle_lines(position.x, position.y, radius + 0.5, 2., BLACK);
+            draw_circle(position.x, position.y, STATE_RADIUS, state_color);
+            draw_circle_lines(position.x, position.y, STATE_RADIUS + 0.5, 2., BLACK);
 
             if fa.automaton.initial() == Some(*state) {
                 draw_arrow(
-                    vec2(position.x - radius, position.y),
+                    vec2(position.x - STATE_RADIUS, position.y),
                     0.,
-                    initial_arrow_size,
+                    INITIAL_ARROW_SIZE,
                     true,
                 );
             }
 
             let text = &state.to_string();
-            let font_size = 30.;
-            let text_size = measure_text(text, None, font_size as _, 1.0);
+            let text_size = measure_text(text, None, STATE_FONT_SIZE as _, 1.0);
             draw_text_ex(
                 &state.to_string(),
                 position.x - text_size.width / 2.,
-                position.y - text_size.height / 2. + radius / 2.,
+                position.y - text_size.height / 2. + STATE_RADIUS / 2.,
                 TextParams {
-                    font_size: font_size as _,
+                    font_size: STATE_FONT_SIZE as _,
                     font,
                     color: BLACK,
                     ..Default::default()
                 },
             );
+        }
+
+        if let Some(from) = creating_transition_from {
+            let position = position_map.get(&from).unwrap_or(&Vec2::ZERO);
+            draw_transition(position, &mouse_position, false);
         }
 
         egui_macroquad::draw();
