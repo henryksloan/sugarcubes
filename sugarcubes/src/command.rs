a0f77abbba2c5ddc2fb8744814d6c647d0e2f990
Henry Sloan <henryksloan@gmail.com>
2021-09-28 11:57:03 -0400

    Make renaming an undoable action

--- sugarcubes/src/command.rs
+++ sugarcubes/src/command.rs
@@ -13,6 +13,9 @@ pub enum Command {
     // The state to be updated, and the new value of is_final
     SetFinal(u32, bool),
 
+    // The state's ID and its old and new names
+    SetStateName(u32, String, String),
+
     // The state's ID and position
     CreateState(u32, Vec2),
     // The transition to be added
@@ -26,18 +29,22 @@ pub enum Command {
 
 impl Command {
     pub fn execute(&self, fa: &mut FiniteAutomaton, states: &mut States) {
-        match *self {
-            Self::SetInitial(state, _) => fa.automaton.set_initial(state),
+        match self {
+            Self::SetInitial(state, _) => fa.automaton.set_initial(*state),
             Self::RemoveInitial(_) => fa.automaton.remove_initial(),
-            Self::SetFinal(state, value) => fa.automaton.set_final(state, value),
+            Self::SetFinal(state, value) => fa.automaton.set_final(*state, *value),
+
+            Self::SetStateName(state, _, new_name) => {
+                states.insert_name(*state, new_name.to_string())
+            }
 
             Self::CreateState(state, pos) => {
-                states.try_add_state_with_id(fa, pos, state);
+                states.try_add_state_with_id(fa, *pos, *state);
             }
-            Self::CreateTransition(transition) => fa.automaton.add_transition(transition),
+            Self::CreateTransition(transition) => fa.automaton.add_transition(*transition),
 
-            Self::DeleteState(state, _, _, _) => states.remove_state(fa, state),
-            Self::DeleteTransition(transition) => fa.automaton.remove_transition(transition),
+            Self::DeleteState(state, _, _, _) => states.remove_state(fa, *state),
+            Self::DeleteTransition(transition) => fa.automaton.remove_transition(*transition),
         }
     }
 
@@ -52,6 +59,10 @@ impl Command {
             }
             Self::RemoveInitial(state) => fa.automaton.set_initial(*state),
             Self::SetFinal(state, value) => fa.automaton.set_final(*state, !value),
+
+            Self::SetStateName(state, old_name, _) => {
+                states.insert_name(*state, old_name.to_string())
+            }
 
             Self::CreateState(state, _) => states.remove_state(fa, *state),
             Self::CreateTransition(transition) => fa.automaton.remove_transition(*transition),
